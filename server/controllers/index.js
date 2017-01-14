0d3c1becf3b1f583b9f557c347fde6dc1b4e36f4
simon-huang <simon.t.huang@gmail.com>
2017-01-14 12:10:37 -0800

    Refactor models and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,40 +1,17 @@
 var models = require('../models');
 var mysql = require('mysql');
-var Promise = require('bluebird');
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      dbConnection = mysql.createConnection({
-        user: 'root',
-        password: '',
-        database: 'chat'
-      });
-      dbConnection.connect();
-      dbConnection.query('SELECT * from messages', function(err, result) {
-        if (err) {
-          console.log('error');
-          throw err;
-        }
-        res.end(JSON.stringify(result));
+      models.messages.get(function(allMessages) {
+        res.end(allMessages);
       });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      dbConnection = mysql.createConnection({
-        user: 'root',
-        password: '',
-        database: 'chat'
-      });
-      dbConnection.connect();
-      console.log({message: req.body.message, username: req.body.username, roomname: req.body.roomname});
-      dbConnection.query('INSERT INTO messages SET ?', {message: req.body.message, username: req.body.username, roomname: req.body.roomname}, function(err, result) {
-        if (err) {
-          console.log('error');
-          throw err;
-        }
+      models.messages.post(req.body, function() {
         res.end();
       });
-      
     } // a function which handles posting a message to the database
   },
 
@@ -42,17 +19,7 @@ module.exports = {
     // Ditto as above
     get: function (req, res) {},
     post: function (req, res) {
-      dbConnection = mysql.createConnection({
-        user: 'root',
-        password: '',
-        database: 'chat'
-      });
-      dbConnection.connect();
-      dbConnection.query('INSERT INTO usernames SET ?', {username: req.body.username}, function(err, result) {
-        if (err) {
-          console.log('error');
-          throw err;
-        }
+      models.users.post(req.body, function() {
         res.end();
       });
     }
--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,41 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (responseEnder) {
+      console.log('trying to get messages');
+      db.query('SELECT * from messages', function(err, result) {
+        if (err) {
+          throw err;
+        }
+
+        responseEnder(JSON.stringify(result));
+      });
+    }, // a function which produces all the messages
+    post: function (message, responseEnder) {
+      console.log('trying to post message');
+
+      db.query('INSERT INTO messages SET ?', {message: message.message, username: message.username, roomname: message.roomname}, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        responseEnder();
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
     get: function () {},
-    post: function () {}
+    post: function (user, responseEnder) {
+      console.log('trying to post users');
+      // console.log('stringified', JSON.stringify(result));
+      db.query('INSERT INTO usernames SET ?', {username: user.username}, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        responseEnder();
+      });
+    }
   }
 };
 
